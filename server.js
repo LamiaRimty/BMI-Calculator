41e1cb32443933b14cbbe0eb32f759d6f8cfe9b6
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-10-10 20:33:40 +0200

    initial files

---
+++ server.js
@@ -0,0 +1,23 @@
+//jshint esversion:6
+const express =require("express");
+const bodyParser=require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+    res.sendFile(__dirname,"/bmiCalculator.html");
+});
+
+app.post("/",function(req,res){
+    var height=Number(req.body.height);
+    var weight=Number(req.body.weight);
+    var result=height/weight;
+    console.log(result);
+       res.send("Thanks for posting");
+     
+   });
+   
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+});
---
+++ wrong.js
@@ -0,0 +1,12 @@
+// //jshint esversion:6
+// const express = require("express");
+// const app=express(); //func represents express module & we bind that with a word app
+
+// app.get("/",function(req,res){
+//     res.send("Hello");
+// });
+
+// app.listen(5000,function(){
+//     console.log("Server is running on port 5000");//build my first Server.it tels to listen on a specific port for any http requests that get sent to our server 
+// });
+
